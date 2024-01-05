043cebdfc04263c629b43b8c88618d866c78a137
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2024-01-05 17:25:15 +0200

    completed add property page

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,7 +1,128 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import "../styles/AddProperty.css";
 
 function AddProperty() {
-  return <div>Add Property Page</div>;
+  const initialState = {
+    fields: {
+      title: "",
+      city: "Manchester",
+      type: "Flat",
+      bedrooms: "",
+      bathrooms: "",
+      price: "",
+      email: "",
+    },
+  };
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+  const handleFieldChange = (event) => {
+    const changedField = event.target.name;
+    const newValue = event.target.value;
+    setFields({ ...fields, [changedField]: newValue });
+  };
+
+  return (
+    <div className="AddProperty">
+      <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Title
+          <input
+            placeholder="Luxury Apartment..."
+            type="text"
+            id="title"
+            name="title"
+            value={fields.title}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="city">
+          City
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Sheffield">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
+          </select>
+        </label>
+        <label htmlFor="type">
+          Type
+          <select
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+          >
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End of Terrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
+        </label>
+        <label htmlFor="bedrooms">
+          Bedrooms
+          <input
+            placeholder="3"
+            type="number"
+            min="0"
+            id="bedrooms"
+            name="bedrooms"
+            value={fields.bedrooms}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="bathrooms">
+          Bathrooms
+          <input
+            placeholder="1"
+            type="number"
+            min="0"
+            id="bathrooms"
+            name="bathrooms"
+            value={fields.bathrooms}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price(£)
+          <input
+            placeholder="123456"
+            type="number"
+            min="0"
+            id="price"
+            name="price"
+            value={fields.price}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            placeholder="john.smith@email.co.uk"
+            type="email"
+            id="email"
+            name="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddProperty;
